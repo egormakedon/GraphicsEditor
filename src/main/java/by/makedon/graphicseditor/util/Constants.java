fec01827bc3e0266623a2a576102a2c865c5b1f7
Yahor Makedon <emakedonw@gmail.com>
2019-12-07 14:47:55 +0300

    refactoring Window class, move images to resources/img

--- src/main/java/by/makedon/graphicseditor/util/Constants.java
+++ src/main/java/by/makedon/graphicseditor/util/Constants.java
@@ -6,6 +6,14 @@ package by.makedon.graphicseditor.util;
 public final class Constants {
     public static final String APPLICATION_PROPERTIES_FILENAME = "application.properties";
 
+    public static final String MENUBAR_COLOR = "menubar.color";
+    public static final String MENUBAR_WIDTH = "menubar.width";
+    public static final String MENUBAR_HEIGHT = "menubar.height";
+
+    public static final String FRAME_NAME = "frame.name";
+    public static final String FRAME_MINIMUM_WIDTH = "frame.minimum.width";
+    public static final String FRAME_MINIMUM_HEIGHT = "frame.minimum.height";
+
     private Constants() {
     }
 }
--- src/main/java/by/makedon/graphicseditor/window/Window.java
+++ src/main/java/by/makedon/graphicseditor/window/Window.java
@@ -1,5 +1,9 @@
 package by.makedon.graphicseditor.window;
 
+import by.makedon.graphicseditor.frame.AddTools;
+import by.makedon.graphicseditor.util.Constants;
+import by.makedon.graphicseditor.util.ResourceUtil;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,24 +14,45 @@ public class Window {
     private JFrame jFrame;
 
     public Window() {
-        JFrame frame = this.frame.getFrame();
-        JMenuBar menuBar = this.menuBar.getMenuBar();
-        JPanel toolBar = this.toolBar.getToolBar();
-        JPanel drawPanel = new NewPanel();
-        JScrollPane scrollPane = new JScrollPane(drawPanel);
-
-        frame.setJMenuBar(menuBar);
-        frame.add(toolBar, BorderLayout.WEST);
-        frame.add(scrollPane, BorderLayout.CENTER);
-
-//        addTools = new AddTools(drawPanel);
-//        addTools.setBars(menuBar, toolBar);
-
-
+        jFrame = createJFrame();
     }
 
     public void show() {
         jFrame.setVisible(true);
+    }
+
+    private JFrame createJFrame() {
+        String frameName = ResourceUtil.getPropertyValue(Constants.FRAME_NAME);
+        int frameMinimumWidth = Integer.parseInt(ResourceUtil.getPropertyValue(Constants.FRAME_MINIMUM_WIDTH));
+        int frameMinimumHeight = Integer.parseInt(ResourceUtil.getPropertyValue(Constants.FRAME_MINIMUM_HEIGHT));
+
+        JFrame jFrame = new JFrame(frameName);
+        jFrame.setMinimumSize(new Dimension(frameMinimumWidth, frameMinimumHeight));
+        jFrame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setLocationRelativeTo(null);
+
+        jFrame.setJMenuBar(createJMenuBar());
+
+//        JPanel drawPanel = new NewPanel();
+//        JScrollPane scrollPane = new JScrollPane(drawPanel);
+//        frame.add(scrollPane, BorderLayout.CENTER);
+//        addTools = new AddTools(drawPanel);
+//        addTools.setBars(menuBar, toolBar);
+
+        return jFrame;
+    }
+
+    private JMenuBar createJMenuBar() {
+        String menuBarColor = ResourceUtil.getPropertyValue(Constants.MENUBAR_COLOR);
+        int menuBarWidth = Integer.parseInt(ResourceUtil.getPropertyValue(Constants.MENUBAR_WIDTH));
+        int menuBarHeight = Integer.parseInt(ResourceUtil.getPropertyValue(Constants.MENUBAR_HEIGHT));
+
+        JMenuBar jMenuBar = new JMenuBar();
+        jMenuBar.setBackground(Color.decode(menuBarColor));
+        jMenuBar.setPreferredSize(new Dimension(menuBarWidth, menuBarHeight));
+        jMenuBar.setBorder(BorderFactory.createEtchedBorder());
+        return jMenuBar;
     }
 
 //    public class NewPanel extends JPanel {
