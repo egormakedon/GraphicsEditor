c25619bbd8d2f37d3da7b396f21df8433d39af77
Yahor Makedon <emakedonw@gmail.com>
2019-12-26 04:25:10 +0300

    add rendering hints

--- src/main/java/by/makedon/graphicseditor/manager/DrawManager.java
+++ src/main/java/by/makedon/graphicseditor/manager/DrawManager.java
@@ -12,6 +12,8 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Yahor Makedon
@@ -96,9 +98,17 @@ public final class DrawManager {
             g.setColor(color);
             g.setStroke(new BasicStroke(thickness));
 
-//            Map<Object, Object> hints = new HashMap<>();
-//            hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//            g.setRenderingHints(hints);
+            Map<Object, Object> hints = new HashMap<>();
+            hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            hints.put(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+            hints.put(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+            hints.put(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+            hints.put(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+            hints.put(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+            hints.put(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+            g.setRenderingHints(hints);
 
             return g;
         }
@@ -150,10 +160,6 @@ public final class DrawManager {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.drawImage(bufferedImage, 0, 0, this);
-
-//            Map<Object, Object> hints = new HashMap<>();
-//            hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//            ((Graphics2D)g).setRenderingHints(hints);
         }
     }
 }
